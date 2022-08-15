a9acd431e3a659a8535075b3d1a35a3f24b349e1
jungdeokwoo <dogsocks@dogsocksui-MacBookPro.local>
2022-08-15 19:31:25 +0900

    로그인 레이아웃

--- src/pages/Todo/TodoList.tsx
+++ src/pages/Todo/TodoList.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 
 const TodoList = () => {
-  return <div>todo리스트</div>;
+  return (
+    <>
+      {localStorage.getItem("token") ?? <Navigate to={"/"} replace />}
+      <div>todo리스트</div>
+    </>
+  );
 };
 
 export default TodoList;
